181f64898a1749ed1daabd4290367dff306e2454
Jakob Flierl <jakob.flierl@gmail.com>
2011-12-18 17:13:06 +0100

    tidy

--- scripts/svg-country-4x3.sh
+++ scripts/svg-country-4x3.sh
@@ -6,7 +6,7 @@
 STYLE="simple flat fancy glossy"
 
 for f in $(cd svg/country-4x3; ls -1 *.svg); do
-    echo -n " build/svg-country-4x3-* $f";
+    echo "build/svg-country-4x3-* $f";
 
     for s in $STYLE; do 
 	scripts/build.pl --cmd svg2svg \
@@ -20,5 +20,5 @@ for f in $(cd svg/country-4x3; ls -1 *.svg); do
 	    --mask 107x107+67x67+1065x745 \
 	    --geo 105x105+1280x960 --geoscale 1.667
     done
-    echo " ok.";
 done
+echo "ok.";
--- scripts/svg-country-squared.sh
+++ scripts/svg-country-squared.sh
@@ -4,7 +4,6 @@ STYLE="simple flat fancy glossy"
 
 for f in $(cd svg/country-squared; ls -1 *.svg); do
     echo "build/svg-country-squared-* $f";
-
     for s in $STYLE; do 
 	scripts/build.pl --cmd svg2svg --out build/svg-country-squared-$s \
 	    --res 512x512 \
@@ -15,5 +14,5 @@ for f in $(cd svg/country-squared; ls -1 *.svg); do
 	    --svg $f \
 	    --mask 57x57+35x35+398x398 --geo 55x55+512x512 --geoscale 0.782
     done
-    echo " ok.";
 done
+echo " ok.";
