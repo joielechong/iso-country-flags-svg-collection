05957c7900368a99aa99dbaab9287505ba2dc4f2
Jakob Flierl <jakob.flierl@gmail.com>
2011-12-18 21:01:14 +0100

    tidy

--- scripts/sheets.sh
+++ scripts/sheets.sh
@@ -1,36 +1,43 @@
 #!/bin/sh
 
-BGS="LightGrey White Black Transparent"
+BGS="lightgrey white black transparent"
 
 echo "Generating sheets .."
 
 for f in $(cd build; ls -d1 png-*); do
-    echo -n " processing build/$f";
 
     for res in $(cd build/$f; ls -d1 res-*); do
-	echo "/$res";
+	echo " processing build/$f/$res";
 
-	for bg in $BGS; do
+	OUTPD="build/$f/sheets";
+	FLAGS="build/$f/$res/??.png";
+
+	RES=`echo $res | sed s/^res-//g`
+	RESX=`echo $RES | sed s/x.*$//g`
+	RESY=`echo $RES | sed s/^.*x//g`
+
+	if [ "$RESX" = "1280" ]; then
+	    echo "  skipping big resolution $RES.";
+	else
+	    for bg in $BGS; do
 #	    echo "   $bg";
-	    OUTPD="build/$f/sheets";
-	    FLAGS="build/$f/$res/??.png";
-	    SHEET="$OUTPD/$bg-$res.png";
-	    RES=`echo $res | sed s/^res-//g`
+		SHEET="$OUTPD/$res-$bg.png";
 
-	    mkdir -p $OUTPD;
-	    echo "  $SHEET";
+		mkdir -p $OUTPD;
+		echo "  $SHEET";
 
-	    montage \
-		-limit memory 64 -limit map 128 \
-		-font DroidSans-Bold.ttf \
-		-pointsize 8 \
-		-geometry $RES! \
-		$FLAGS \
-		-label "%f" \
-		-fill Black \
-		-background $bg \
-		$SHEET;
-	done
+		montage \
+		    -limit memory 512 -limit map 512 \
+		    -font DroidSans-Bold.ttf \
+		    -pointsize 8 \
+		    -geometry $RES! \
+		    $FLAGS \
+		    -label "%f" \
+		    -fill Black \
+		    -background $bg \
+		    $SHEET;
+	    done
+	fi
     done
 done
 echo "done."
