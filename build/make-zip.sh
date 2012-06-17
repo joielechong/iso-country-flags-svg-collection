e3b457cc74032954b24c66626c3f2fb74eb12a93
Jakob Flierl <jakob.flierl@gmail.com>
2012-06-17 11:11:42 +0200

    tidy

--- build/make-zip.sh
+++ build/make-zip.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 mkdir -p zip
 
-for d in $(ls -d1 png-country-squared-*); do for r in $(ls -d1 $d/res-*); do cp -r $r zip/`echo $r | sed -e s#/#-#`; zip -r zip/`echo $r | sed -e s#/#-#`.zip zip/`echo $r | sed -e s#/#-#`; rm -r $ zip/`echo $r | sed -e s#/#-#`; done; done
+for d in $(ls -d1 png-country-squared-* png-country-4x2-*); do
+  for r in $(ls -d1 $d/res-*); do
+    cp -r $r zip/`echo $r | sed -e s#/#-#`;
+    cd zip
+    zip -r `echo $r | sed -e s#/#-#`.zip `echo $r | sed -e s#/#-#`;
+    cd ..
+    rm -r zip/`echo $r | sed -e s#/#-#`;
+  done;
+done
